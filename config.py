23069f5e49df6717e651509b9973cd6bcd29e15a
fasdxoox <163231340+fasdxoox@users.noreply.github.com>
2024-07-21 23:15:17 +0300

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ API_HASH = getenv("API_HASH","986cb4ba434870a62fe96da3b5f6d411")
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME","")
 
-Muntazer = getenv("muntazer", "eo_u7")
+Muntazer = getenv("muntazer", "YMMYC")
 
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://abood:king@cluster0.rbp1cqz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/LLLA1/NHAY3",
+    "https://github.com/LLLA1/SAUD",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 APK = 5140000000
@@ -43,9 +43,9 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-CHANNEL_NAME = getenv("CHANNEL_NAME", "• 𝐒𝐨𝐮𝐫𝐜𝐞 𝐋𝐚𝐫𝐢𝐧 🎧")
-CHANNEL_LINK = getenv("CHANNEL_LINK", "https://t.me/eer_5o")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/sourcelarin")
+CHANNEL_NAME = getenv("CHANNEL_NAME", "♪ 𝒔𝒂𝒖𝒅 ♪")
+CHANNEL_LINK = getenv("CHANNEL_LINK", "https://t.me/YMMYC")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/YMMYC")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
