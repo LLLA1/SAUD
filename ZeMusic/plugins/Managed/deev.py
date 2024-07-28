4882e75e334a2bcaa7ed2d49fe1e6d086de4ca3b
fasdxoox <163231340+fasdxoox@users.noreply.github.com>
2024-07-28 04:48:32 +0300

    Update deev.py

--- ZeMusic/plugins/Managed/deev.py
+++ ZeMusic/plugins/Managed/deev.py
@@ -13,7 +13,7 @@ async def huhh(client: Client, message: Message):
     name = dev.first_name
 
     await message.reply(
-        text=f"""<b>• Dev ↦  <a href="https://t.me/{usrnam}"> {name}</a></b>""",
+        text=f"""<b>  <b>\n<a href="https://t.me/{usrnam}">• Dev ↦ {name}</a></b>""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
